eba3f4c657700bbcc250262168b7facdf02fe3bf
candela7306 <cecinav883@iesmarquesdecomares.org>
2026-01-26 09:30:01 +0100

    login

--- eventia-front/routes/web.php
+++ eventia-front/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Livewire\HomePage;
+use App\Livewire\Auth\Login;
 
 Route::get('/', HomePage::class);
+
+Route::get('/login', Login::class)->name('login');
+Route::view('/dashboard', 'dashboard')->name('dashboard');
+Route::view('/profile', 'profile')->name('profile.edit');
