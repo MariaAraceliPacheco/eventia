94ee787c1c23884de64755ec262da40642686e5d
AraceliPacheco <maricelipachecoarjona18@gmail.com>
2026-02-01 21:00:40 +0100

    relacion usuario con artista

--- eventia-front/app/Models/User.php
+++ eventia-front/app/Models/User.php
@@ -67,4 +67,8 @@ class User extends Authenticatable
             ->map(fn($word) => Str::substr($word, 0, 1))
             ->implode('');
     }
+
+    public function perfilArtista() {
+        return $this->hasOne(Artista::class, 'id_usuario');
+    }
 }
