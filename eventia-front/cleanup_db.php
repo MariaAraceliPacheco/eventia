ca1af39929108f909459628ec6e204ed4ae98deb
MariaAraceliPacheco <maricelipachecoarjona18@gmail.com>
2026-02-19 12:22:23 +0100

    entorno limpiado. Archivos innecesarios eliminados

--- eventia-front/cleanup_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-Illuminate\Support\Facades\Schema::dropIfExists('solicitudes');
-Illuminate\Support\Facades\Schema::dropIfExists('solicitudes_debug');
-echo "Tables dropped successfully.\n";
